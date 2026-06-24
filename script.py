38533b297e7b6e2c3b769404f6128f5f2b5ec52f
hana bader <hanabader281@gmail.com>
2026-06-24 22:29:37 +0530

    Build and verify an interactive integer addition calculator

--- script.py
+++ script.py
@@ -1,2 +1,6 @@
-name = "Hana"
-print("Hello " + name + "! Let's crack GSoC, LFX, and elite hackathons.")
+num1 =int(input("Enter your first mark: "))
+num2 =int(input("Enter your second mark: "))
+
+total_marks = num1+num2
+
+print("Your total combined mark is: " + str(total_marks))
