131908676dcdd5d2ea525eb067e2030258711d2f
hana bader <hanabader281@gmail.com>
2026-06-26 18:48:39 +0530

    Successfully build e-commerce math algorithm and styled CSS UI portfolio layout

--- script.py
+++ script.py
@@ -1,11 +1,13 @@
-groceries = ["Milk", "Eggs", "Apple"]
+cart_prices = [100,250,50,500,120]
+total_cost = 0
 
-print(groceries)
+for price in cart_prices:
 
+    if price > 200:
+        discounted_price = price * 0.90
+        total_cost = total_cost + discounted_price
+    else:
+        total_cost = total_cost + price
 
-print("The first item is: " + groceries[0])
+print("The final smart cart total is: INR " + str(total_cost))
 
-groceries.append("Banana")
-
-print("Updated list:")
-print(groceries)
